711b41cb825e35a21f46d0e2bed23fbe77b1eacb
AdrienBEH <171994@supinfo.com>
2018-01-01 07:12:26 +0100

    CI

--- crawl.sh
+++ crawl.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
 if [ "./docs" ]; then
         rm -r ./docs
 fi
-=======
->>>>>>> ff07ac7724a2ee5f26f554ca5da4d587738b1258
 ./node_modules/casperjs/bin/casperjs --verbose --cookies-file=cookies.txt app.js
--- fix-footer.sh
+++ fix-footer.sh
@@ -3,7 +3,4 @@
 ./cpdf -crop "0mm 8mm 300mm 300mm" ./docs/doc.pdf -o doc_fixed.pdf
 ./cpdf -add-text "Page %Page sur %EndPage" -bottomright 30pt -font "Times-Roman" -font-size 9 doc_fixed.pdf -o doc_fixed.pdf
 ./cpdf -compress doc_fixed.pdf -o doc_fixed.pdf
-<<<<<<< HEAD
 echo "doc_fixed.pdf created :)"
-=======
->>>>>>> ff07ac7724a2ee5f26f554ca5da4d587738b1258
