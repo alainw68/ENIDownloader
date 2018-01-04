185ade3eafdc8430dbe791a9d0541e8c7b9856a5
Adrien BEHADIR <adrien.behadir@supinfo.com>
2018-01-04 02:48:45 +0100

    ADD:Information about ./docs operations

--- crawl.sh
+++ crawl.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-if [ "./docs" ]; then
-        rm -r ./docs
+if [ -d ./docs ]; then
+        rm -r ./docs && echo "./docs erased!"
 fi
+echo -n "./docs/ <= Download..."
 ./node_modules/casperjs/bin/casperjs --verbose --cookies-file=cookies.txt app.js
